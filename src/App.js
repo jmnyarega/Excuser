4c5a3a359dea5f67823f0aca4822e5991ac6c2d3
josiahmokob0 <jmnyarega@gmail.com>
2021-11-22 16:50:15 +0300

    - I LOVE EXCUSES

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+const random = (max, min = 0) => Math.floor(Math.random() * (max - min) + min);
+const trim = (str, length) => str?.substr(0, str.length - length);
+
+const randomHue = random(360);
+const color = `hsl(${randomHue}, 50%, 50%)`;
+const URL =
+  "https://gist.githubusercontent.com/josiahmokob0/0e4f7dca37b33dc1ef395dab6bb2b105/raw/33f9b0378ad92697fde16b11e22755ea6273a37a/excuses.json";
+
+const styles = {
+  main: {
+    backgroundColor: color,
+    color,
+  },
+  button: {
+    backgroundColor: color,
+    color: "white",
+  },
+};
 
 function App() {
+  const [excuse, setExcuse] = useState();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fecthData = async () => {
+      const res = await axios.get(URL);
+      setData(res.data);
+      setExcuse(res.data[random(res.data.length)]);
+    };
+    fecthData();
+  }, []);
+
+  const newExcuse = () => {
+    setExcuse(data[random(data.length)]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <main style={styles.main}>
+      <div id="quote-box">
+        <p id="text">
+          <i className="fa fa-quote-left"> </i>
+          {excuse?.excuse}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <p id="author"> - {trim(excuse?.reason, 11)}</p>
+
+        <div className="buttons">
+          <button id="new-quote" onClick={newExcuse} style={styles.button}>
+            New Excuse
+          </button>
+        </div>
+      </div>
+    </main>
   );
 }
 
